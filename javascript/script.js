d025cb0f454626c66be7907416c50ebd271586b4
shakir hussain <sh0552802@gmail.com>
2022-09-13 02:21:17 +0530

    portfolio website

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+/*========== type animation ==========*/
+var typed = new Typed (".profession",{ 
+    strings:["Front-end Developer","Skilling Towards Full Stack Developer",],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
+
+
